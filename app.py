685ee73fb43421583858639851bd09429ed5a3d8
Marco Vaessen <marco.vaessen@gmail.com>
2025-04-09 19:02:28 +0200

    Updated app to bind to 0.0.0.0 for deployment

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def create_db_tables():
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
     
 
